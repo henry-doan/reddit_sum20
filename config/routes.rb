08254c87c2a3f5d9914e1b96c8122664fb18a541
Henry Doan <henrydoan96@gmail.com>
2020-06-01 21:01:25 -0600

    sub index and new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,37 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'subs#index'
+
+  # get 'sub/index'
+  # get 'sub/edit'
+  # get 'sub/show'
+  # get 'sub/new'
+  # post 'sub/create'
+
+  resources :subs do
+    resources :topics  
+      # bad practice
+      # resources :comments do
+      #   resources :likes
+      # end
+
+    # '/subs/:sub_id/topics'
+    # /subs/1/topics
+
+    # /subs/:sub_id/topics/:id
+    # /subs/2/topics/1
+  end
+
+  resources :topics do
+    resources :comments
+  end
+
+  # resources :sub, only: [:index, :show, :new]
+
+  # resources :pictures
+
+  # http verb  'url pattern', to: 'controller#action'
+    #  :id   placeholder for id 
+    # get '/people', to: 'people#index'
+    # "/subs/3"
+
 end
